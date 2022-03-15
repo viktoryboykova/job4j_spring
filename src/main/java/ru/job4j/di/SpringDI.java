56c41295c71386c4a065bbc3c7c0be5884caca81
viktoryboykova <viktoryboykova@list.ru>
2022-03-15 15:44:54 +0300

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -7,9 +7,13 @@ public class SpringDI {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.scan("ru.job4j.di");
         context.refresh();
-        StartUI ui = context.getBean(StartUI.class);
-        ui.add("Petr Arsentev");
-        ui.add("Ivan ivanov");
-        ui.print();
+//        StartUI ui = context.getBean(StartUI.class);
+//        ui.add("Petr Arsentev");
+//        ui.add("Ivan ivanov");
+//        ui.print();
+        Store store = context.getBean(Store.class);
+        store.add("Petr Arsentev");
+        Store another = context.getBean(Store.class);
+        another.getAll().forEach(System.out::println);
     }
 }
